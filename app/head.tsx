39804e5fdea2c2546ade1f7cb4edc37125ab06c8
Bastian Treitz <bastian.treitz@web.de>
2022-12-06 23:45:27 +0100

    Add redirect to root

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,11 @@
 export default function Head() {
 	return (
 		<>
-			<title>trtz.dev</title>
-			<meta name="description" content="Bastian Treitz - Software Developer" />
+			<title>trtz.dev - Bastian Treitz</title>
+			<meta
+				name="description"
+				content="Bastian Treitz - Software Developer - Personal website to display information on my favorite and latest public projects using technologies that interest me."
+			/>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<link rel="icon" href="/images/default-icon.png" />
 		</>
--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
+	async redirects() {
+		return [
+			{
+				source: "/:slug+",
+				destination: "/",
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
